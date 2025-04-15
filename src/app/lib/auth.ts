90dc5c2598c2053c58859a60d730cfa4a36656ff
joaoev <joaoalvesxp@alu.ufc.br>
2025-04-15 01:30:17 -0300

    feat: implementando firebase para salvar login do usuário

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,7 +1,11 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter"
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { firebaseCert } from "./firebase"
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [Google],
-  
+  adapter: FirestoreAdapter({
+    credential: firebaseCert
+  })
 })
